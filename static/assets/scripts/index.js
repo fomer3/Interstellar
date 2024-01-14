703edd1acd3729e2ee69072ba613ec2a77a5eb2f
fomer3 <123983699+fomer3@users.noreply.github.com>
2024-01-14 10:32:45 -0600

    Update index.js

--- static/assets/scripts/index.js
+++ static/assets/scripts/index.js
@@ -59,5 +59,5 @@ if (password==pass1)
   alert('Password Correct. Click OK to enter!');
 else
    {
-    window.location.replace("https://www.classroom.google.com");
+    window.location.replace("https://www.google.com");
     }
